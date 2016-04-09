bd29aaeff6c8a70ba7cbb50e76c2d16acc6619b2
Bryan Newby <brnewby602@gmail.com>
2016-04-08 17:35:40 -0700

    Add initial grunt file and dist directory. Update package.json for dependencies

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    
+    clean: ['client/dist'],
+
+    concat: {
+      options: {
+        separator: ';\n',
+      },
+      js: {
+        src: ['client/www/js/*.js'],
+        dest: 'client/dist/<%= pkg.name %>.js',
+      },
+      css: {
+        src: ['client/www/css/*.css'],
+        dest: 'client/dist/<%= pkg.name %>.css',
+      } 
+    },
+
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'client/dist/<%= pkg.name %>.min.js': ['client/dist/<%= pkg.name %>.js']
+        }
+      }
+    }
+
+
+    
+  });
+
+  
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+
+  grunt.registerTask('build', [ 
+    'clean', 'concat', 'uglify'
+  ]);
+
+
+};
