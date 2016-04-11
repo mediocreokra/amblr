6e0e414279a34fb50112886e9b3505f476aaf8c6
Bryan Newby <brnewby602@gmail.com>
2016-04-11 13:51:15 -0700

    Add tab and left menu in index.html, add leftNavController

--- client/www/js/app.js
+++ client/www/js/app.js
@@ -3,8 +3,12 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('amblr', ['ionic', 'ngCordova', 'amblr.map', 'amblr.services'])
-
+angular.module('amblr', 
+  ['ionic', 
+   'ngCordova', 
+   'amblr.leftnav',
+   'amblr.map', 
+   'amblr.services'])
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
@@ -22,7 +26,6 @@ angular.module('amblr', ['ionic', 'ngCordova', 'amblr.map', 'amblr.services'])
     }
   });
 })
-
 .config(function($stateProvider, $urlRouterProvider) {
 
   $stateProvider
@@ -41,3 +44,5 @@ angular.module('amblr', ['ionic', 'ngCordova', 'amblr.map', 'amblr.services'])
   $urlRouterProvider.otherwise("/");
 
 });
+
+
