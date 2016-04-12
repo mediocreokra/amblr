c26db18c1fa3c3e4ce2f4f838db78099919730e2
Alex Nitta <a.j.nitta@gmail.com>
2016-04-12 09:48:01 -0700

    Clean up linting issues, edits to Gruntfile for testing env setup

--- client/www/js/services.js
+++ client/www/js/services.js
@@ -3,9 +3,9 @@ angular.module ('amblr.services', [])
 .factory('POIs', function($http) {
   var POIs = {};
 
-  POIs.getPOIs = function(){
+  POIs.getPOIs = function() {
     return $http.get('http://127.0.0.1:3000/api/allPOI')
-    .then(function(pois){
+    .then(function(pois) {
       console.log('returning pois are: ', pois);
       return pois;
     })
