3444fa6474e412b32a7c496cb0efcfe726aa1f32
Trini Le <trinile@gmail.com>
2016-04-13 14:09:16 -0700

    merge conflicts

--- client/www/js/app.js
+++ client/www/js/app.js
@@ -5,7 +5,8 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('amblr', [
   'ionic', 
-  'ngCordova', 
+  'ngCordova',
+  'uiGmapgoogle-maps', 
   'amblr.map', 
   'amblr.directives',
   'amblr.leftnav',
@@ -29,8 +30,12 @@ angular.module('amblr', [
     }
   });
 })
-.config(function($stateProvider, $urlRouterProvider) {
-
+.config(function($stateProvider, $urlRouterProvider, uiGmapGoogleMapApiProvider) {
+  uiGmapGoogleMapApiProvider.configure({
+    key: 'AIzaSyBceRLiJZrDWlQiK3vu2Mc6-gzp84ZQX5U',
+    v: '3.20', //defaults to latest 3.X anyhow
+    libraries: 'weather,geometry,visualization'
+  });
   $stateProvider
   .state('menu', {
     url: '/menu',
