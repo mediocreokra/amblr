a1eee498f8293c09428f55666073285151af5db4
Trini Le <trinile@gmail.com>
2016-04-14 14:09:29 -0700

    fixing merge conflicts in map controller

---
+++ client/www/js/controllers/addPoiController.js
@@ -0,0 +1,11 @@
+angular.module('addPOI', ['uiGmapgoogle-maps'])
+.controller('addPOIController', function($scope, $ionicModal, POIs,$log, $timeout) {
+
+  //current POI that is selected
+  $scope.currentPOI = {};
+
+  $scope.getCurrentPOI = function(POI) {
+    $scope.currentPOI = POI;
+  };
+
+});
