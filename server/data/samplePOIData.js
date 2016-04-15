d134ca9724f275eddb09bb6998c4cd7afc1c90b5
Bryan Newby <brnewby602@gmail.com>
2016-04-15 12:24:08 -0700

    Update type to use lowercase values in sampleData

--- server/data/samplePOIData.js
+++ server/data/samplePOIData.js
@@ -2,14 +2,14 @@ var poiData = [
   {
     'lat': 37.783184,
     'long': -122.408467,
-    'type': 'Good',
+    'type': 'good',
     'description': 'Band playing on street.',
     'title': 'I am a title 1'
   },
   {
     'lat': 37.784210,
     'long': -122.410130, 
-    'type': 'Bad',
+    'type': 'bad',
     'description': 'Inebriated hoard is rampaging in the street.',
     'title': 'I am a title 2'
   }
