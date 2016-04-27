7b4766ac09d01bf4f3e25c8592df526db65dfd5e
Alex Nitta <a.j.nitta@gmail.com>
2016-04-26 21:12:13 -0700

    Modify SSL cert files

--- server/server.js
+++ server/server.js
@@ -49,8 +49,8 @@ app.use('/api/users', userRouter);
 
 
 var options = {
-  key: fs.readFileSync('./config/keys/key.pem'),
-  cert: fs.readFileSync('./config/keys/server.crt')
+  key: fs.readFileSync('./config/keys/privKey.pem'),
+  cert: fs.readFileSync('./config/keys/cert.pem')
 };
 
 // Create an HTTPS service 
